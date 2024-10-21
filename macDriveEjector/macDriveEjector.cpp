779f16ccbf76b8d670a37379f9d990ed762548ba
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2024-10-21 15:21:38 +0100

    Add support for third drive (USB SuperDrive)

--- macDriveEjector/macDriveEjector.cpp
+++ macDriveEjector/macDriveEjector.cpp
@@ -7,20 +7,24 @@
 
 bool isDrive1Open = false;
 bool isDrive2Open = false;
-wchar_t driveLetter1, driveLetter2;
+bool isDrive3Open = false;
+
+wchar_t driveLetter1, driveLetter2, driveLetter3;
 
 void toggleDrive(wchar_t driveLetter, bool& isDriveOpen) {
     wchar_t drivePath[7];
     wsprintf(drivePath, L"\\\\.\\%c:", driveLetter);
 
     HANDLE hDrive = CreateFile(drivePath, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
-    if (hDrive == INVALID_HANDLE_VALUE) {
+    if (hDrive == INVALID_HANDLE_VALUE) 
+    {
         std::wcout << L"Failed to open drive " << driveLetter << L":" << std::endl;
         return;
     }
 
     DWORD bytesReturned;
-    if (isDriveOpen) {
+    if (isDriveOpen) 
+    {
         if (!DeviceIoControl(hDrive, IOCTL_STORAGE_LOAD_MEDIA, NULL, 0, NULL, 0, &bytesReturned, NULL)) {
             std::wcout << L"Failed to close drive " << driveLetter << L":" << std::endl;
         }
@@ -29,7 +33,8 @@ void toggleDrive(wchar_t driveLetter, bool& isDriveOpen) {
             isDriveOpen = false;
         }
     }
-    else {
+    else 
+    {
         if (!DeviceIoControl(hDrive, IOCTL_STORAGE_EJECT_MEDIA, NULL, 0, NULL, 0, &bytesReturned, NULL)) {
             std::wcout << L"Failed to eject drive " << driveLetter << L":" << std::endl;
         }
@@ -45,12 +50,17 @@ void toggleDrive(wchar_t driveLetter, bool& isDriveOpen) {
 int main() {
 
     system("color 70");
+    std::wcout << "Welcome to the macDriveEjector!" << std::endl;
 
     std::ifstream inFile("drive_letters.txt");
     if (!inFile)
     {
         std::cerr << "Unable to read drive letters";
         exit(1);
+    }
+    else
+    {
+        std::wcout << "Drive Letters retrieved successfully!" << std::endl;
     }
 
     char temp;
@@ -60,14 +70,25 @@ int main() {
     inFile >> temp;
     driveLetter2 = (wchar_t)toupper(temp);
 
+    inFile >> temp;
+    driveLetter3 = (wchar_t)toupper(temp);
+
     while (true) {
-        if (GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(VK_F3)) {
-            std::wcout << L"Ejected bay A" << std::endl;
-            toggleDrive(driveLetter1, isDrive1Open); // Toggle first drive
+        if (GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(VK_F2))
+        {
+            std::wcout << L"Ejected USB SuperDrive" << std::endl;
+            toggleDrive(driveLetter1, isDrive1Open); // Toggle USB SuperDrive
+            std::wcout << L"Please note that the USB SuperDrive cannot be remotely closed as it is a slimline drive." << std::endl;
         }
-        else if (GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(VK_F4)) {
+        else if (GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(VK_F3)) 
+        {
+            std::wcout << L"Ejected bay A" << std::endl;
+            toggleDrive(driveLetter2, isDrive2Open); // Toggle first drive
+        }
+        else if (GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(VK_F4)) 
+        {
             std::wcout << L"Ejected bay B" << std::endl;
-            toggleDrive(driveLetter2, isDrive2Open); // Toggle second drive
+            toggleDrive(driveLetter3, isDrive3Open); // Toggle second drive
         }
         Sleep(100); // Sleep to reduce CPU usage
     }
